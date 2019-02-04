4aa6a4102e2bd0f25de9c92f38e329eecfaa02f3
Samyon Shahnovitch <samyon.v@gmail.com>
2019-02-04 21:56:48 +0200

    Add files via upload

---
+++ res/sct.js
@@ -0,0 +1,45 @@
+function allInvisible() {
+	document.getElementById("load").style.display = "none";
+	document.getElementById("start").style.display = "none";
+	document.getElementById("host").style.display = "none";
+	document.getElementById("join").style.display = "none";
+	document.getElementById("wait").style.display = "none";
+	document.getElementById("play").style.display = "none";
+}
+function toLoad() {
+	allInvisible();
+	document.getElementById("load").style.display = "table-cell";
+	document.getElementById("container").style.height = "230px";
+}
+
+function toStart() {
+	allInvisible();
+	document.getElementById("start").style.display = "table-cell";
+	document.getElementById("container").style.height = "200px";
+}
+
+function toHost() {
+	allInvisible();
+	document.getElementById("host").style.display = "table-cell";
+	document.getElementById("container").style.height = "450px";
+}
+
+function toJoin() {
+	allInvisible();
+	document.getElementById("join").style.display = "table-cell";
+	document.getElementById("container").style.height = "300px";
+}
+
+function toWait() {
+	allInvisible();
+	document.getElementById("wait").style.display = "table-cell";
+	document.getElementById("container").style.height = "500px";
+}
+
+function toPlay() {
+	allInvisible();
+	document.getElementById("play").style.display = "table-cell";
+	document.getElementById("container").style.height = "500px";
+}
+
+
