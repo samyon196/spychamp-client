9778669bd51cc4fd7c88d9da3fa7c13831f8a667
samyon196 <samyon.v@gmail.com>
2019-02-05 18:52:28 +0200

    Trying work out the css :( :@ :$

--- build.py
+++ build.py
@@ -6,12 +6,12 @@ js = ""
 for sub, dirs, files in os.walk("./views/"):
 	for d in dirs:
 		with open('views/' + d + '/' + d + '.html') as file:
-			html += (file.read() + '\n') 
+			html += (file.read() + '\n')
 			css += '\t<link rel="stylesheet" type="text/css" href="views/' + d + '/' + d + '.css" />\n'
 			js += '\t<script src="views/' + d + '/' + d + '.js"></script>\n'
 
 
-with open('index-template.html', 'r') as file :
+with open('html/index.html', 'r') as file :
 	filedata = file.read()
 	filedata = filedata.replace('{{views-css-link}}', css)
 	filedata = filedata.replace('{{views-js-link}}', js)
