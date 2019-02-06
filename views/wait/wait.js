fc7d8bf49805381225fc3b29e2472691f2cdff2a
Samyon Shahnovitch <samyon.v@gmail.com>
2019-02-06 21:06:17 +0200

    Almost working waiting

--- views/wait/wait.js
+++ views/wait/wait.js
@@ -1,17 +1,26 @@
 function addUserToList(name) {
-
-
-
+    var listcont = document.getElementById("plist");
+    listcont.style.height = listcont.offsetHeight;
     var firstname = document.getElementById('newnick').value;
     var entry = document.createElement('li');
     entry.appendChild(document.createTextNode(firstname));
     var list = document.getElementById("pplist");
+    listcont.style.height = listcont.offsetHeight + 'px';
     list.appendChild(entry);
+    listcont.style.height = listcont.offsetHeight + 'px';
+    /*
     var waiting = document.getElementById("wait");
 	var header = document.getElementById("header");
-	var container = document.getElementById("container");
+    var container = document.getElementById("container");
+    var listcont = document.getElementById("plist");
+    var listheight = list.offsetHeight + 'px';
+    listcont.style.height = listheight;
 	var height = waiting.scrollHeight + header.scrollHeight + 'px';
     container.style.height = height;
+
+*/
+    
     pplist = document.getElementById("pplist");
     pplist.scrollTop = pplist.scrollHeight;
+    openView("wait");
 }
