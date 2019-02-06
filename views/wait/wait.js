da508f2987a49ccf2654714dd630e16e5ef7b344
Samyon Shahnovitch <samyon.v@gmail.com>
2019-02-06 20:15:21 +0200

    Finished wait view

---
+++ views/wait/wait.js
@@ -0,0 +1,17 @@
+function addUserToList(name) {
+
+
+
+    var firstname = document.getElementById('newnick').value;
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(firstname));
+    var list = document.getElementById("pplist");
+    list.appendChild(entry);
+    var waiting = document.getElementById("wait");
+	var header = document.getElementById("header");
+	var container = document.getElementById("container");
+	var height = waiting.scrollHeight + header.scrollHeight + 'px';
+    container.style.height = height;
+    pplist = document.getElementById("pplist");
+    pplist.scrollTop = pplist.scrollHeight;
+}
